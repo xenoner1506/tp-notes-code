b2f62a1dad2ae6d116af759a33fe946595100851
Vitalii <zavadskiy.vs@phystech.edu>
2024-04-18 10:59:17 +0300

    addd docker guides

--- week11/01-simple_web/app.py
+++ week11/01-simple_web/app.py
@@ -4,15 +4,15 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route("/")
 def hello_world():
     return 'Hello, World!'
 
 
 @app.route('/about')
 def about():
-    return 'This is the about page'
+    return '<H1><u>This is the about page</u></H1>'
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host="0.0.0.0")
